6343c188f952280e0d780d99bda4b9a0d500fd49
Jm <weeeezyl@gmail.com>
2018-02-27 18:53:56 +0800

    Update feb27react.js

--- reactpract/components/feb27react.js
+++ reactpract/components/feb27react.js
@@ -1,44 +1,57 @@
 import React, { Component } from 'react';
 
 //State and lifecycle 
+function ShowTime(props){
+	//console.log(props);
+	return <p>{props.date.toLocaleTimeString()}</p>;
+}
 
 class Clock extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {date: new Date()};
+		console.log(this.state);
 	}
-
 	tick(){
 		this.setState({
 			date: new Date()
 		});
 	}
-	//componentDidMount is a reference from reactDOM;
+
 	componentDidMount(){
 		this.timerID = setInterval(()=>this.tick(), 1000);
 	}
-	//componentWillUnmout is a reference from ReactDOM;
+
 	componentWillUnmount(){
 		clearInterval(this.timerID);
 	}
 
 	render(){
-		return (
-			<div>
-				<h1>It is {this.state.date.toLocaleTimeString()}</h1>
-			</div>
-			);
+		return (<ShowTime date={this.state.date} />);
 	}
 }
 
 // function Tick(){
 // 	return <Clock />;
 // }
-
+// function Run(){
+// 	return (<div>	
+// 			<Clock />
+// 			<Clock />
+// 			<Clock />
+// 			</div>
+// 			);
+// }
 
 class Run extends Component{
 	render(){
-		return <Clock />;
+		return (
+			<div>
+			<div><p>First</p><Clock /></div>
+			<Clock />
+			<Clock />
+			</div>
+			);
 	}
 }
 
