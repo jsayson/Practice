789e105f0e32f59ba8cfe749536c8703ab47b193
Jm <weeeezyl@gmail.com>
2018-02-28 16:53:39 +0800

    Add files via upload

---
+++ reactpract/components/feb28react.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+//Handling events
+
+// function Clicked(e){
+// 	e.preventDefault();
+// 	console.log('this Button was clicked.');
+// }
+
+// class Clicker extends Component{
+// 	render(){
+// 		return <button onClick={/*console.log('hello')*/Clicked}>Fire</button>;
+// 	}
+// }
+
+// class Toggle extends Component{
+// 	constructor(props){
+// 		super(props);
+// 		this.state = {
+// 			isToggleOn: true
+// 		};
+// 		this.handleClick = this.handleClick.bind(this);
+// 	}
+// 	handleClick(){
+// 		console.log(this.state);
+// 		this.setState((prevState) => ({
+// 			isToggleOn: !prevState.isToggleOn
+// 		}));
+// 	}
+// 	render(){
+// 		return (<div><button onClick={this.handleClick}>
+// 		{this.state.isToggleOn ? 'On' : 'Off'}
+// 		</button></div>);
+// 	}
+// }
+class StartWar extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			firing: false,
+			callGen: true
+		}
+		console.log(props)
+	}
+	calledGen(){
+		this.setState(this.firing === true ? 'Call' : 'Called');
+	}
+	render(){
+		return <h1>{this.calledGen}</h1>;
+	}
+}
+
+class Toggle extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			firing : false
+		}
+		this.firingClick = this.firingClick.bind(this);
+	}
+	firingClick(){
+		this.setState(gunFiring => ({
+			firing: !gunFiring.firing
+		}));
+	}
+	render(){
+		return (<div><button className='firing' onClick={this.firingClick}>
+			{this.state.firing ? 'Ceasefire' : 'Firing'}
+			</button>
+			<StartWar fire={this.state.firing} /></div>);
+	}
+}
+
+export default Toggle;
