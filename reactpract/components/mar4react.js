4c2a08a058ffe121e0d35e5da34e408ce3f7d81f
Jm <weeeezyl@gmail.com>
2018-03-04 16:03:32 +0800

    Update mar4react.js

--- reactpract/components/mar4react.js
+++ reactpract/components/mar4react.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 
 class MultiForm extends Component{
+	constructor(props){
+		super(props);
+		this.state = { goingTo : true, 
+			numberOfGuests: 0
+		};
+		this.handleThisInputChange = this.handleThisInputChange.bind(this);
+	}
+	handleThisInputChange(event){
+		let value = event.target.type === 'number' ? event.target.value : event.target.checked;
+		// let name = event.target.name;
+		this.setState({
+			[event.target.name] : value
+		});
+	}
 	render(){
-		return <h1>Hello WOrld</h1>;
+		return (<form>
+			<label>No. of Going:
+			<input type='checkbox' name='goingTo' checked={this.state.goingTo} onChange={this.handleThisInputChange} />
+			</label>
+			<br />
+			<label>
+			Number of Guests:
+			<input type='number' name='numberOfGuests' value={this.state.numberOfGuests} onChange={this.handleThisInputChange} />
+			</label>
+			</form>);
 	}
 }
 
