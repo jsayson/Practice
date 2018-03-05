add4f6e298ea946cf064c4c4b7e1bae651732c1c
Jm <weeeezyl@gmail.com>
2018-03-05 15:48:19 +0800

    Update mar5react.js

--- reactpract/components/mar5react.js
+++ reactpract/components/mar5react.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
 
+function Temp(props){
+	console.log(props);
+	if(props.deg >= 35){
+		return <h1>The Weather is hot</h1>;
+	}
+	return <h1>Such a nice Weather</h1>;
+}
+
+// const temp = { location: 'Philippines',
+// 	temperature: 30	
+// }
+
+class CalculateTemp extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			temperature: ''
+		}
+		this.handleThisChange = this.handleThisChange.bind(this);
+	}
+	handleThisChange(e){
+		this.setState({temperature: e.target.value});
+	}
+	render(){
+		const temp = this.state.temperature;
+		return (<fieldset>
+			<legend>Enter temperature in Celius: </legend>
+			<input value={temp} onChange={this.handleThisChange} />
+			<Temp deg={parseFloat(temp)} />
+			</fieldset>
+			);
+	}
+}
+
 class LiftingState extends Component{
 	render(){
-		return (<h1>Hello World!</h1>);
+		return (<CalculateTemp />)
 	}
 }
 
