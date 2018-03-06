32d42a1f45ccbf8490714c62b82dd8f29694f73e
Jm <weeeezyl@gmail.com>
2018-03-06 15:42:13 +0800

    Update mar6react.js

--- reactpract/components/mar6react.js
+++ reactpract/components/mar6react.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+function FancyBorder(props){
+	console.log(props);
+	return (<div className={props.color}>
+		{props.children}
+		</div>);
+}
+
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
+}
 
 class Containment extends Component{
 	render(){
-		return <h1>Hello World</h1>
+		return (<WelcomeDialog />);
 	}
 }
 
