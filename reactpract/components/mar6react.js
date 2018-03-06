b20893e6380f7eb34e4f915b0b87822484517513
Jm <weeeezyl@gmail.com>
2018-03-06 18:06:20 +0800

    Update mar6react.js

--- reactpract/components/mar6react.js
+++ reactpract/components/mar6react.js
@@ -40,21 +40,51 @@ import React, { Component } from 'react';
 //     		</div>);
 // }
 
-function Webpage(props){
-	console.log(props.children[0].props.className);
-	return (<div className='page'>{props.children}</div>);
+// function Webpage(props){
+// 	console.log(props.children[0].props.className);
+// 	return (<div className='page'>{props.children}</div>);
+// }
+
+// function Conversation(props){
+// 	//console.log(props);
+// 	return (<Webpage>
+// 		<div className='title'><h1>{props.title}</h1></div>
+// 		<div className='content'><p>{props.content}</p></div>
+// 		</Webpage>);
+// }
+
+// function PageContent(){
+// 	return (<Conversation title='This is the title' content='This is the content' />)
+// }
+
+function Dialog(props){
+	console.log(props.children[0].props.value);
+	return (<div className='content'><div className='about'><h1>{props.title}</h1><p>{props.message}</p></div>
+		<div className='inputs'>{props.children}</div></div>);
 }
 
-function Conversation(props){
-	//console.log(props);
-	return (<Webpage>
-		<div className='title'><h1>{props.title}</h1></div>
-		<div className='content'><p>{props.content}</p></div>
-		</Webpage>);
-}
+class PageContent extends Component{
+	constructor(props){
+		super(props);
+		this.state = { message : ''}
+		this.handleThisChange = this.handleThisChange.bind(this);
+		this.handleThisOutput = this.handleThisOutput.bind(this);
+	}
 
-function PageContent(){
-	return (<Conversation title='This is the title' content='This is the content' />)
+	handleThisChange(e){
+		this.setState({message : e.target.value});
+	}
+
+	handleThisOutput(){
+		console.log(this.state.message);
+		alert(this.state.message === 'yes' ? (this.state.message.charAt(0).toUpperCase()+this.state.message.slice(1)) + '?, then you\'re going to DIE!' : 'Want to learn magic?');
+	}
+	render(){
+		return (<Dialog title='Learn Magic' message='Are you a mage?'>
+			<input value={this.state.message} onChange={this.handleThisChange} />
+			<input type='submit' onClick={this.handleThisOutput} value='submit'/>				
+			</Dialog>);
+	}
 }
 
 class Containment extends Component{
