6f59887def992f0916e466919e5e91f95a98a3e0
Jm <weeeezyl@gmail.com>
2018-03-11 14:49:06 +0800

    Update mar10react.js

--- reactpract/components/mar10react.js
+++ reactpract/components/mar10react.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import '../css/mar10css.css'; 
 
 const style = {
-	color: 'red',
+	color: 'pink',
 	fontSize: '36px',
 	fontWeight: 'bold'
 }
@@ -13,17 +13,30 @@ const style = {
 // 	console.log(style.color);
 // 		return(<div className='test'>The Color of this word is <span style={style}>{style.color}</span></div>);
 // 	}
-// }
-const MyComponent = {
-	ComDesign : (props) => {
-		console.log(props); 
-		return (<div className='test'>The color is <span style={style}>{style.color}</span>. </div>);
-	}
-}
+// }\
 
+// const MyComponent = {
+// 	ComDesign : (props) => {
+// 		console.log(props); 
+// 		return (<div className='test'>The color is <span style={style}>{style.color}</span>. </div>);
+// 	}
+// }
+
+// class Try extends Component{
+// 	render(){
+// 		return <MyComponent.ComDesign style={style}/>
+// 	}
+// }
+function Solve(props){
+	var sum = parseInt(props.sum, 0);
+	//console.log(props);
+	var ans = null;
+	var val = (sum) => {ans = (<div className='test'>The sum is: <span style={props.resColor}>{(sum+=1)}</span></div>); return ans;};
+	setInterval(val, 1000);
+}
 class Try extends Component{
 	render(){
-		return <MyComponent.ComDesign style={style}/>
+		return <Solve sum={1+1+1+1} resColor={style} />
 	}
 }
 export default Try;
