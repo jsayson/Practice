fde601903397be16fb46c47f5d28e45a1a1a9514
Jm <weeeezyl@gmail.com>
2018-03-12 16:49:52 +0800

    Update mar12react.js

--- reactpract/components/mar12react.js
+++ reactpract/components/mar12react.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TypeChecking extends Component{
+	static defaultProps = {
+		name: 'guest'
+	}
+
 	render(){
-		return <h1>Hello World</h1>;
+		const name = this.props.name || 'admin';
+		return (<h1>{String(name) === 'undefined' ? 'No current user.' : 'Welcome, '+name.charAt(0).toUpperCase()+name.slice(1)}</h1>);
 	}
 }
+
+// TypeChecking.defaultProps = {
+// 	name: 'guest'
+// }
+
+TypeChecking.propTypes = {
+	name: PropTypes.string.isRequired,
+	customProp: function(props, propName, componentName) {
+    if (!/matchme/.test(props[propName])) {
+      return new Error(
+        'Invalid prop `' + propName + '` supplied to' +
+        ' `' + componentName + '`. Validation failed.'
+      );
+    }
+  }
+};
 
 export default TypeChecking;
