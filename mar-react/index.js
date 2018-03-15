a22afc0e5742a4dfe0ce83d513ac3b4fe042f4e3
Jm <weeeezyl@gmail.com>
2018-03-15 16:38:10 +0800

    Update index.js

--- mar-react/index.js
+++ mar-react/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+// import * as React from 'react';
 import ReactDOM from 'react-dom';
+//import PropTypes from 'prop-types';
 
 //css
 import './css/mar14.css';
 
-class Show extends React.Component{
+//js
+import Flowify from './js/mar14react.js';
+import DefProp from './js/mar15react.js';
+
+// class TestProps extends React.Component{
+// 	static propTypes = {
+// 		num: PropTypes.number.isRequired
+// 	}
+// 	render(){
+// 		if(typeof (this.props.num) === 'number'){
+// 		return (<h1>{this.props.num}</h1>);
+// 		}
+// 		return (<h1>Not a valid input</h1>);
+// 	}
+// }
+
+
+class Webpage extends React.Component{
 	render(){
-		return (<h1>Hello World</h1>);
+		return (<DefProp />);
 	}
 }
 
-ReactDOM.render(<Show />, document.getElementById('root'));
+ReactDOM.render(<Webpage />, document.getElementById('root'));
