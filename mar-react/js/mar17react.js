cfbfb88d4944a15da525fb5cd4f46222989a54bb
Jm <weeeezyl@gmail.com>
2018-03-17 17:25:29 +0800

    Update mar17react.js

--- mar-react/js/mar17react.js
+++ mar-react/js/mar17react.js
@@ -1,18 +1,38 @@
 import * as React from 'react';
 import { Component } from 'react';
 
-type Props = {
-	greeting: string
-}
+import '../css/mar14.css';
+// type Props = {
+// 	greeting: string
+// }
 
-class MComponent extends Component<Props>{
-	static MyProp = {
-		greeting: 'Hello World'
+// class MComponent extends Component<Props>{
+// 	render(){
+// 	// console.log(props);
+// 		return (<h1>{this.props.greeting}</h1>);
+// 	}
+// }
+
+class MComponent extends Component{
+	constructor(props){
+		super(props);
+		this.focusThisText = this.focusThisText.bind(this);
+	}
+
+	focusThisText(){
+		let myInput = document.getElementById('input');
+		myInput.classList['add']('effect');
 	}
 
 	render(){
-		return (<h1>{props.greeting}</h1>);
+		return (<div><input type='text' id='input' onClick={this.focusThisText} className='add' /></div>)
 	}
 }
 
-export default MComponent;
+class MyComponent extends Component{
+	render(){
+		return <MComponent greeting='Hello World' />;		
+	}
+}
+
+export default MyComponent;
