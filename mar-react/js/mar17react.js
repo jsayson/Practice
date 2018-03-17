da85e98a1ef47cfa791a8c2322a1deb942c2f222
Jm <weeeezyl@gmail.com>
2018-03-17 18:29:46 +0800

    Update mar17react.js

--- mar-react/js/mar17react.js
+++ mar-react/js/mar17react.js
@@ -32,15 +32,26 @@ import '../css/mar14.css';
 class MComponent extends Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			greeting: '',
+			status: true,
+		}
 		this.focusThisText = this.focusThisText.bind(this);
 	}
 	focusThisText(){
 		let myInput = document.getElementById('input');
 		myInput.classList['add']('effect');	
 		this.textInput.focus();
+		//let greetChange = 
+		this.setState({
+			greeting: this.state.status === true ? 'Hi Ho' : 'Hello World' ,
+			status: this.state.status === true ? false : true
+		});
 	}
 	render(){
 		return(<div>
+			<h1>{this.state.greeting === '' ? 'Hello World' : this.state.greeting }</h1>
+			<p>{	String(this.state.status)}</p>
 			<input type='text' id='input' className='add' ref={(input)=>{this.textInput = input}} />
 			<input value='Focus the text input' type='submit' onClick={this.focusThisText} />
 			</div>);
