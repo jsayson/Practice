1d6370d70db9e5da559758da425b19fe6caa3b69
Jm <weeeezyl@gmail.com>
2018-03-19 15:23:30 +0800

    Update mar18react.js

--- mar-react/js/mar18react.js
+++ mar-react/js/mar18react.js
@@ -1,10 +1,42 @@
 import * as React from 'react';
 import { Component } from 'react';
 
+
 class MyComponent extends Component{
-	render(){
-		return (<h1>Hello World</h1>);
-	}
+	constructor(props) {
+    super(props);
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    // Explicitly focus the text input using the raw DOM API
+    this.textInput.focus();
+  }
+
+  render() {
+    // Use the `ref` callback to store a reference to the text input DOM
+    // element in an instance field (for example, this.textInput).
+    return (
+      <div>
+        <input type="text" ref={(input) => { this.textInput = input; }} />
+        
+      </div>
+    );
+  }
 }
 
-export default MyComponent;
+class AutoFocusTextInput extends Component {
+  componentDidMount() {
+    this.textInput.focusTextInput();
+  }
+
+  render() {
+    return (
+      <MyComponent ref={(input) => { this.textInput = input; }} />
+    );
+  }
+}
+
+
+
+export default AutoFocusTextInput;
