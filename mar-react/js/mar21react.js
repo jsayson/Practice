c3a0ac728dae860ac415781910211ed0d88651f5
Jm <weeeezyl@gmail.com>
2018-03-21 17:04:08 +0800

    Update mar21react.js

--- mar-react/js/mar21react.js
+++ mar-react/js/mar21react.js
@@ -2,9 +2,40 @@ import * as React from 'react';
 import { Component } from 'react';
 
 class MyComponent extends Component{
-	render(){
-		return <h1>Hello World</h1>;
+	// handleInput = () => {
+	// 	let name=this.myInput.value;
+	// 	this.myInput.focus();
+	// 	console.log(name);
+	// 	name = '';
+	// }
+	// render(){
+	// 	return (<div>
+	// 		<input type='text' defaultValue='kira' ref={input=> this.myInput = input} />
+	// 		<input type='submit' value='submit'  onClick={this.handleInput} />
+	// 		</div>);
+	// }
+	constructor(props) {
+    	super(props);
+    	this.handleSubmit = this.handleSubmit.bind(this);
+  	}
+  	handleSubmit(event) {
+    	event.preventDefault();
+    	console.log(this.fileInput.files[0]);
 	}
+  	render() {
+    	return (
+    	<div>
+      	<form onSubmit={this.handleSubmit}>
+        	<label>
+          	Upload file:
+          	<input type="file" ref={input => {this.fileInput = input;}} />
+        	</label>
+        	<br />
+        	<button type="submit">Submit</button>
+      	</form>
+      	</div>
+    	);
+  	}
 }
 
 export default MyComponent;
