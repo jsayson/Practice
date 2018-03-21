5a25a4198d244450b0b29a8a784679babec80f8e
Jm <weeeezyl@gmail.com>
2018-03-21 15:55:24 +0800

    Add files via upload

---
+++ mar-react/js/mar21react.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+class MyComponent extends Component{
+	render(){
+		return <h1>Hello World</h1>;
+	}
+}
+
+export default MyComponent;
