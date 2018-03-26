4229dd490182c1c06c35c8092377504a8cccfafa
Jm <weeeezyl@gmail.com>
2018-03-26 16:55:11 +0800

    Update section2.js

--- react-web/Components/section2.js
+++ react-web/Components/section2.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 
-class Section2 extends Component{
+//import pic
+import calc from '../pic/calc.jpg';
+import udacity from '../pic/udacity.jpg';
+import twitch from '../pic/twitchApi.png';
+import search from '../pic/search.jpg';
+import generate from '../pic/generate.png';
+import weather from '../pic/weather.png';
+
+const creations = [
+{
+	imgSrc: calc,
+},
+{
+	imgSrc: udacity,
+},
+{
+	imgSrc: twitch,
+},
+{
+	imgSrc: search,
+},
+{
+	imgSrc: generate,
+},
+{
+	imgSrc: weather,
+}
+];
+
+class Port extends Component{
 	render(){
 		return <h1>Hello World</h1>
+	}
+}
+
+class Section2 extends Component{
+	render(){
+		return <Port images={creations} />
 	}
 }
 
