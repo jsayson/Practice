3fd7f28cb94458241079cecff94317c3b6f5fd65
Jm <weeeezyl@gmail.com>
2018-03-27 17:07:34 +0800

    Update section3.js

--- react-web/Components/section3.js
+++ react-web/Components/section3.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
+
+const about = {
+	address : {
+	 	blk: 20,
+	 	lot: 5,
+	 	st: 'Philippians St.',
+	 	brgy: 'San Antonio',
+	 	city: 'San Pedro',
+	 	province: 'Laguna',
+	},
+	phone: '+639453828681',
+	email: 'josemarisayson@gmail.com'
+}
+
+class Info extends Component{
+	render(){
+		//console.log(this.props);
+		const add = this.props.add;
+		return (<div>
+			<p>Blk {add.blk} Lot {add.lot} {add.st} {add.brgy} {add.city} {add.province}</p>
+			<p>{this.props.email}</p>
+			<p>{this.props.phone}</p>
+			</div>);
+	}
+}
+
 class Section3 extends Component{
 	render(){
-		return <h1>Hello World</h1>
+		return <Info phone={about.phone} email={about.email} add={about.address} />
 	}
 }
 
