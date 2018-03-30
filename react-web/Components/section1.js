7fea91b2f49bc9103c49627fc3ffd0c0a2114dae
Jm <weeeezyl@gmail.com>
2018-03-30 19:43:49 +0800

    Update section1.js

--- react-web/Components/section1.js
+++ react-web/Components/section1.js
@@ -40,28 +40,41 @@ class SocialMed extends Component{
 		// 	// console.log(val.name);
 		// 	<Row key={index} name={index} />
 		// })}</ul>);
-		console.log(this.props.social);
+		// console.log(this.props.social);
 		let res = this.props.social.map((res,index)=>{
-			return <a key={index} className={res.className} href="###"></a>
+			return <a key={index} className={res.className} href={res.link}></a>
 		})
 		return (<div>{res}</div>);
 	}
 }
 
 const magic = [
-	{name: 'codepen', className: 'fa fa-codepen'},
-	{name: 'freecodecamp', className: 'fa fa-free-code-camp'},
-	{name: 'github', className: 'fa fa-github'},
-	{name: 'instagram', className: 'fa fa-instagram'},
-	{name: 'linkedin', className: 'fa fa-linkedin'},
-	{name: 'twitter', className: 'fa fa-twitter'}
+	{name: 'codepen', className: 'fa fa-codepen', link: 'https://codepen.io/Sssrnty'},
+	{name: 'freecodecamp', className: 'fa fa-free-code-camp', link: 'https://www.freecodecamp.org/sssrnty'},
+	{name: 'github', className: 'fa fa-github', link: 'https://github.com/sssrnty'},
+	{name: 'instagram', className: 'fa fa-instagram', link: 'https://instagram.com/sssrnty'},
+	{name: 'linkedin', className: 'fa fa-linkedin', link: 'https://www.linkedin.com/in/jm-sayson'},
+	{name: 'twitter', className: 'fa fa-twitter', link: 'https://twitter.com/Sssrnty'}
 ];
 
 class About extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			viewed: false
+		}
+		this.imViewed = this.imViewed.bind(this);
+	}
+	imViewed(e){
+		this.setState({
+			viewed: !this.state.viewed
+		});
+		e.preventDefault();
+	}
 	render(){
 		const val = this.props.info;
-		return (<div><div><img src={val.imgSrc} alt={val.alt} height={val.height} width={val.width}/></div>
-			<div className='intro'><h1>The Name is Jm</h1></div>
+		return (<div  id='section1'><div className={this.state.viewed === false ? 'click' : 'clicked' }><img src={val.imgSrc} alt={val.alt} height={val.height} width={val.width} onClick={this.imViewed}/></div>
+			<div className='intro'><p>The Name is Jm</p></div>
 			<div className='links'><SocialMed social={magic}/></div>
 			</div>)	
 	}
