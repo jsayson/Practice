5bd0dd18fc873ed8a7be0f6775e5613205c0f130
Jm <weeeezyl@gmail.com>
2018-04-20 20:43:06 +0800

    Update index.js

--- cra-exp-post-prac/front-end/src/index.js
+++ cra-exp-post-prac/front-end/src/index.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import axios from 'axios';
+import axios from 'axios';
 
 class App extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			title: ''
+			title: '',
+			year: '',
+			imdb: '',
+			r_imdb: '',
+			r_tomato: ''
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	handleChange(e){
-        const state = this.state
+	handleChange = (e) => {
+        const state = this.state;
         state[e.target.name] = e.target.value;
         this.setState(state);
 	}
 	handleSubmit(event){
 		event.preventDefault();
+		console.log(this.state);
+		const { title, year, imdb, r_imdb, r_tomato } = this.state;
 		fetch('/submitted', {
 			method: 'POST',
 			body: JSON.stringify({
-		      title: this.state.title
+		      'title': title,
+		      'year': year,
+		      'imdb': imdb,
+		      'r_imdb': r_imdb,
+		      'r_tomato': r_tomato,
 		    }),
 		    headers: {"Content-Type": "application/json"}
 		  })
 	}
 	render(){
+		const { title, year, imdb, r_imdb, r_tomato } = this.state;
 		return (
 			<form onSubmit={this.handleSubmit} name='form'>
-			<input type='text' name='title' placeholder='Title' onChange={this.handleChange}/>
+			<input type='text' name='title' placeholder='Title' value={title} onChange={this.handleChange}/><br/>
+			<input type='text' name='year' placeholder='Year' value={year} onChange={this.handleChange}/><br/>
+			<input type='text' name='imdb' placeholder='IMDB' value={imdb} onChange={this.handleChange}/><br/>
+			<label>Rating<br/>
+			<input type='text' name='r_imdb' placeholder='IMDB' value={r_imdb} onChange={this.handleChange}/><br/>
+			<input type='text' name='r_tomato' placeholder='Tomato' value={r_tomato} onChange={this.handleChange}/><br/>
+			</label>
 			<input type='button' value='submit' />
 			</form>
 			);
