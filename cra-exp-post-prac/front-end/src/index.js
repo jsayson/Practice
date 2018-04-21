54d7f7dd8c997881cfa14fcd2b3f7a3bc6260e07
Jm <weeeezyl@gmail.com>
2018-04-21 18:21:05 +0800

    Update index.js

--- cra-exp-post-prac/front-end/src/index.js
+++ cra-exp-post-prac/front-end/src/index.js
@@ -1,83 +1,48 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 
 class App extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			title: '',
-			year: '',
-			imdb: '',
-			r_imdb: '',
-			r_tomato: ''
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	handleChange = (e) => {
-        const state = this.state;
-        state[e.target.name] = e.target.value;
-        this.setState(state);
-	}
-	handleSubmit(event){
-		event.preventDefault();
-		console.log(this.state);
-		const { title, year, imdb, r_imdb, r_tomato } = this.state;
-		fetch('/submitted', {
-			method: 'POST',
-			body: JSON.stringify({
-		      'title': title,
-		      'year': year,
-		      'imdb': imdb,
-		      'r_imdb': r_imdb,
-		      'r_tomato': r_tomato,
-		    }),
-		    headers: {"Content-Type": "application/json"}
-		  })
-	}
-	render(){
-		const { title, year, imdb, r_imdb, r_tomato } = this.state;
-		return (
-			<form onSubmit={this.handleSubmit} name='form'>
-			<input type='text' name='title' placeholder='Title' value={title} onChange={this.handleChange}/><br/>
-			<input type='text' name='year' placeholder='Year' value={year} onChange={this.handleChange}/><br/>
-			<input type='text' name='imdb' placeholder='IMDB' value={imdb} onChange={this.handleChange}/><br/>
-			<label>Rating<br/>
-			<input type='text' name='r_imdb' placeholder='IMDB' value={r_imdb} onChange={this.handleChange}/><br/>
-			<input type='text' name='r_tomato' placeholder='Tomato' value={r_tomato} onChange={this.handleChange}/><br/>
-			</label>
-			<input type='button' value='submit' />
-			</form>
-			);
-	}
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const dataItems = {
+      title: data.get('title'),
+      year: data.get('year'),
+      imdbLink: data.get('imdbLink'),
+      r_tomato: data.get('r_tomato'),
+      r_imdb: data.get('r_imdb')
+    }
+    console.log(dataItems);
+    fetch('/api/submitted', {
+      method: 'POST',
+      body: JSON.stringify(dataItems),
+      headers: {'Content-Type': 'application/json'}
+    });
+    let inputs = document.querySelectorAll('#inputs');
+    console.log(inputs);
+    for(let val of inputs){
+      val.value = '';
+    }
+    inputs[0].focus();
+  }
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit} onLoad={this.handleSubmit}>
+      <input type='text' name='title' placeholder='Title' id='inputs'/><br/>
+      <input type='text' name='year' placeholder='Year' id='inputs'/><br/>
+      <input type='text' name='imdbLink' placeholder='Imdb Link' id='inputs'/><br/>
+      <label>Rating<br />
+      <input type='text' name='r_tomato' placeholder='Tomato' id='inputs'/><br/>
+      <input type='text' name='r_imdb' placeholder='Imdb' id='inputs'/><br/>
+      </label>
+      <input type='submit' value='submit' />
+      </form>
+      );
+  }
 }
-
-// class App extends React.Component{
-// 	constructor(props){
-// 		super(props);
-// 		this.state = {
-// 			items: ''
-// 		}
-// 		this.handleSubmit = this.handleSubmit.bind(this);
-// 	}
-// 	handleSubmit(e){
-// 		e.preventDefault();
-// 		let data = new FormData(e.target)
-// 		fetch('/submitted', {
-// 			method: 'post',
-// 			body: JSON.stringify(data),
-// 			headers: {"Content-Type": "application/json"}
-// 		});
-// 	}
-// 	render(){
-// 		return (
-// 			<form onSubmit={this.handleSubmit}>
-// 			<input type='text' placeholder='title' name='title' />
-// 			<input type='button' value='submit' />
-// 			</form>
-// 			);
-// 	}
-// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
