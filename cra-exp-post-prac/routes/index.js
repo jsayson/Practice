81065c45cf986039a7b828364622feb657095700
Jm <weeeezyl@gmail.com>
2018-04-24 19:29:11 +0800

    Update index.js

--- cra-exp-post-prac/routes/index.js
+++ cra-exp-post-prac/routes/index.js
@@ -1,5 +1,11 @@
 const indexRoute = require('./indexRoute.js');
+const searchRoute = require('./searchRoute.js');
+const moviesRoute = require('./moviesRoute.js');
+const mdRoute = require('./mdRoute.js');
 
 module.exports = function(app, db){
 	indexRoute(app, db);
+	// searchRoute(app, db);
+	moviesRoute(app, db);
+	mdRoute(app, db);
 };
