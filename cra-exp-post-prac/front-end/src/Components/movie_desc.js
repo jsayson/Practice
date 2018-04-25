e6437ea02a4e1d2134fd0405fba9f4f3e1da1c25
Jm <weeeezyl@gmail.com>
2018-04-25 17:09:54 +0800

    Update movie_desc.js

--- cra-exp-post-prac/front-end/src/Components/movie_desc.js
+++ cra-exp-post-prac/front-end/src/Components/movie_desc.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'; 
 
 class MovieDesc extends React.Component{
 	constructor(props){
@@ -9,6 +10,7 @@ class MovieDesc extends React.Component{
 			error: false,
 			id: this.props.match.params.id
 		}
+		this.handleDelete = this.handleDelete.bind(this);
 	}
 	componentDidMount(){
 		// const { match : { params } } = this.props;
@@ -17,6 +19,12 @@ class MovieDesc extends React.Component{
 		// console.log(this.props.match.params.id.toString());
 		// console.log(url+id);
 		fetch(`${url+this.state.id}`).then(res=>res.json()).then(result=>this.setState({item: result, isLoaded: true})),(error=>this.setState({isLoaded: true, error}));
+	}
+	handleDelete(e){
+		e.preventDefault();
+		fetch(`/api/movies/${this.state.id}`, {
+			method: 'DELETE',
+		}).then(res=>{res.json});
 	}
 	render(){
 		const {item, isLoaded, error } = this.state;
@@ -36,6 +44,7 @@ class MovieDesc extends React.Component{
 			return (<div id={item.length}>
 				<h1>{doc.title}</h1>
 				<p><u>{doc.year}</u><br/><br/> Ratings <br/>Tomato: {doc.rating.tomato}<br/>Imdb: {doc.rating.imdb}</p>
+				<Link to='/movies'><input type='submit' value='Delete' onClick={this.handleDelete} /></Link>
 				</div>);
 		}
 	}
