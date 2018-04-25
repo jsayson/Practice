7cae9c0f62cf36eadb11104e6c998140ba9deb2d
Jm <weeeezyl@gmail.com>
2018-04-25 15:33:08 +0800

    Update movie_desc.js

--- cra-exp-post-prac/front-end/src/Components/movie_desc.js
+++ cra-exp-post-prac/front-end/src/Components/movie_desc.js
@@ -6,25 +6,37 @@ class MovieDesc extends React.Component{
 		this.state = {
 			item: [],
 			isLoaded: false,
-			error: false
+			error: false,
+			id: this.props.match.params.id
 		}
 	}
 	componentDidMount(){
 		// const { match : { params } } = this.props;
 		// console.log(match);
-		fetch('/api/movies/5add9ba7b6266e3428cc5e78').then(res=>res.json()).then(result=>this.setState({item: result, isLoaded: true})),(error=>this.setState({isLoaded: true, error}));
+		const url = '/api/movies/';
+		// console.log(this.props.match.params.id.toString());
+		// console.log(url+id);
+		fetch(`${url+this.state.id}`).then(res=>res.json()).then(result=>this.setState({item: result, isLoaded: true})),(error=>this.setState({isLoaded: true, error}));
 	}
 	render(){
-		const {isLoaded, item, error } = this.state;
-		console.log(item);
+		const {item, isLoaded, error } = this.state;
+		// console.log(item);
+		// console.log(error);
+		// console.log(isLoaded);
 		if(error){
+			console.log(error.message);
 			return <div>Error 404: Content Not Found</div>
 		}
 		else if(!isLoaded){
-			return <div>Loading...</div>
+				return <div>Loading...</div>;
 		}
 		else{
-		return <h1>Hello World</h1>
+			let doc = item[0];
+			// console.log(item.length)
+			return (<div id={item.length}>
+				<h1>{doc.title}</h1>
+				<p><u>{doc.year}</u><br/><br/> Ratings <br/>Tomato: {doc.rating.tomato}<br/>Imdb: {doc.rating.imdb}</p>
+				</div>);
 		}
 	}
 }
