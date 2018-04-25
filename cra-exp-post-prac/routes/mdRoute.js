4fbbd6c5234641214585689f0f436c84c5b5b495
Jm <weeeezyl@gmail.com>
2018-04-25 17:12:02 +0800

    Update mdRoute.js

--- cra-exp-post-prac/routes/mdRoute.js
+++ cra-exp-post-prac/routes/mdRoute.js
@@ -4,8 +4,18 @@ module.exports = function(app, db){
 	app.get('/api/movies/:id', (req, res)=>{
 		let id = req.params['id'];
 		// res.send({'id': id});
+		// console.log('id: '+id);
 		dbc.collection('movies').find({'_id': ObjectId(id)}).toArray((err, result)=>{
 			res.json(result);
 		})
+	});
+	app.delete('/api/movies/:id', (req, res)=>{
+		let id = req.params['id'];
+		let details = { '_id': ObjectId(id) }
+		console.log(details);
+		dbc.collection('movies').remove(details, (err, docs)=>{
+			if(err) throw err;
+			res.json({'hello': 'world'});
+		});
 	});
 };
