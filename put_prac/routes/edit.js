b663ac7b7b30ffaf865173adb597bd285a95f35c
Jm <weeeezyl@gmail.com>
2018-04-28 16:22:45 +0800

    Update edit.js

--- put_prac/routes/edit.js
+++ put_prac/routes/edit.js
@@ -2,18 +2,27 @@ const ObjectId = require('mongodb').ObjectId;
 module.exports = function(app, db){
 	const dbc = db.db('try');
 	app.get('/:edit', (req, res)=>{
-		dbc.collection('posts').find({'_id': new ObjectId(req.query.id)}).toArray((err, docs)=>{
+		// res.send(req.query.id);
+		const details = {'_id': ObjectId(req.query.id) };
+		dbc.collection('posts').find(details).toArray((err, docs)=>{
 			if(err){
-				throw err;
-				res.render('index', {});
+				res.sendStatus(404);
 			}
 			else{
 				res.render('edit', {'item': docs});
 			}
 		});
+		// res.send(details);
 	});
 	app.put('/:edit', (req, res)=>{
-		console.log(req.params);
-		res.send('hi');
+		const details = {'_id': ObjectId(req.query.id) };
+		const note = { $set: {'title': req.query.title, 'desc': req.query.desc} };
+		dbc.collection('posts').updateOne(details, note, (err, result)=>{
+			if (err) {
+		          console.log({'error':'An error has occurred'});
+		      } else {
+		          console.log(note);
+		      } 
+		});
 	});
 };
