2a4af01d720338d84e6e44931fd863e9656a2c4e
Jm <weeeezyl@gmail.com>
2018-05-02 17:04:10 +0800

    Update umdRoute.js

--- cra-exp-post-prac/routes/umdRoute.js
+++ cra-exp-post-prac/routes/umdRoute.js
@@ -1,7 +1,7 @@
 const ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db){
 	const dbc = db.db('try');
-	app.get('/edit/:id', (req, res)=>{
+	app.get('/api/edit/:id', (req, res)=>{
 		const id = req.params.id;
 		const details = {'_id': new ObjectID(id)};
 		dbc.collection('movies').findOne(details, (err, docs)=>{
@@ -9,19 +9,26 @@ module.exports = function(app, db){
 			res.json(docs);
 		});
 	})
-	app.put('/edit/:id', (req, res)=>{
-		const itemId = req.body.id;
-		const itemDetails = { $set : {
-			title: req.body.title,
-			rating: {
-				r_imdb: req.body.r_imdb,
-				r_tomato: req.body.r_tomato,
-			},
-			desc: req.body.description
-		} }; 
-		dbc.collection('movies').updateOne(itemId, itemDetails, (err, docs)=>{
+	app.put('/api/edit/:id', (req, res)=>{
+		// const itemId = req.body.id;
+		// console.log(req.body)
+		// const itemDetails = { $set : {
+		// 	title: req.body.title,
+		// 	rating: {
+		// 		r_imdb: req.body.r_imdb,
+		// 		r_tomato: req.body.r_tomato,
+		// 	},
+		// 	desc: req.body.description
+		// } }; 
+		// dbc.collection('movies').updateOne(itemId, itemDetails, (err, docs)=>{
+		// 	if(err) throw err;
+		// 	res.send({'Updated': docs});
+		const id = {'_id' : new ObjectID(req.body.id)};
+		const edit = { $set: { title : req.body.title} };
+		dbc.collection('movies').updateOne(id, edit, (err, docs)=>{
 			if(err) throw err;
-			res.send({'Updated': docs});
-		})
+			res.send('Updated');
+		});
+		// })
 	})
 };
