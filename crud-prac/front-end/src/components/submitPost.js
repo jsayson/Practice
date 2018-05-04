6f0a37592ef1d4a22e31ee8fe9574cc3560ac22c
Jm <weeeezyl@gmail.com>
2018-05-04 18:33:19 +0800

    Add files via upload

---
+++ crud-prac/front-end/src/components/submitPost.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import '../css/forms.css';
+
+class SubmitPost extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleSubmit(e){
+		e.preventDefault();
+		const data = new FormData(e.target);
+		const item = { title : data.get('title') || 'Empty field' ,description: data.get('description') || 'Empty field'};
+		console.log(item);
+
+	}
+	render(){
+		return (
+			<form onSubmit={this.handleSubmit}>
+			<input type='text' name='title' placeholder='Title'/><br/>
+			<textarea name='description' placeholder='Description'></textarea><br/>
+			<input type='submit' value='Post' />
+			</form>
+			);
+	}
+}
+
+export default SubmitPost;
