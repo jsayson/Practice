be1e0c09c4d3aaf8f9e3a49e63b80c4dcc5675b2
Jm <weeeezyl@gmail.com>
2018-05-05 18:02:22 +0800

    Update index.js

--- crud-prac/routes/index.js
+++ crud-prac/routes/index.js
@@ -1,5 +1,11 @@
 const viewPost = require('./viewPost.js');
+const postId = require('./postId.js');
+const submitPost = require('./submitPost.js');
+const editPost = require('./editPost.js');
 
 module.exports = function(app, db){
 	viewPost(app, db);
+	postId(app, db);
+	submitPost(app, db);
+	editPost(app, db);
 };
