f8c473e311cf72a2997166d1bbb0c76aa4740569
Jm <weeeezyl@gmail.com>
2018-05-12 18:04:03 +0800

    Update createUser.js

--- crud-prac/front-end/src/components/createUser.js
+++ crud-prac/front-end/src/components/createUser.js
@@ -7,45 +7,27 @@ import { Redirect } from 'react-router';
 class CreateAcc extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {
-			user: '',
-			redirect: false
-		}
+		this.state = {};
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	handleSubmit(e){
 		e.preventDefault();
 		const data = new FormData(e.target);
-		const item = {
-			user: data.get('user') || 'test',
-			pass: data.get('pass') || 'test'
-		};
-		fetch('/api/create/user',{
+		const item = { user: data.get('user'), pass: data.get('pass') };
+		fetch('/api/create/user', {
 			method: 'POST',
 			body: JSON.stringify(item),
 			headers: { 'Content-Type' : 'application/json' }
-		}).then(res => res.ok === true ? console.log('Submitted') : false).then(res=>this.setState({user: item.user, redirect: true}));
+		}).then(res=>console.log(res.ok === true ? 'Created Account' : 'failed'));
 	}
 	render(){
-		const { redirect, user } = this.state;
-			if(redirect){
-				return(<div>
-				<Header user={user} />
-				<Redirect to='/' />
-				</div>)
-			}
-			else{
-				return (
-					<div>
-					<form onSubmit={this.handleSubmit}>
-					<label><strong>Create Account</strong></label><br/>
-					<input type='text' name='user' placeholder='Username' /><br/>
-					<input type='password' name='pass' placeholder='Password'/><br/>
-					<input type='submit' value='Submit' />
-					</form>
-					</div>);
-			}
+		return (<form>
+			<label><strong>Create Account</strong></label><br/>
+			<input type='text' placeholder='Username' name='user' /><br/>
+			<input type='password' placeholder='Password' name='pass' /><br/>
+			<input type='submit' value='Submit' />
+			</form>);
+		}
 	}
-}
 
 export default CreateAcc;
