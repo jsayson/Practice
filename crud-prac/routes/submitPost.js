1f71991f7a3d9b808ee32b1b60b15ce282349b78
Jm <weeeezyl@gmail.com>
2018-05-15 18:35:38 +0800

    Update submitPost.js

--- crud-prac/routes/submitPost.js
+++ crud-prac/routes/submitPost.js
@@ -1,10 +1,13 @@
 module.exports = function(app, db){
 	const dbc = db.db('try');
+	var sess;
 	app.post('/api/post', (req, res)=>{
-		const items = { title: req.body.title, description: req.body.description };
+		sess = req.session;
+		const items = { title: req.body.title, description: req.body.description, user: sess.user };
 		dbc.collection('posts').insertOne(items, (err, docs)=>{
 			if(err) throw err;
-			console.log(docs);
+			console.log({user: sess.user}, docs);
+			res.send('posted');
 		});
 	});
 };
