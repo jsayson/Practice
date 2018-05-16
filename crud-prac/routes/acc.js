8eaf1dfed713177f0ba93f2f9b80fd8590a7d9dd
Jm <weeeezyl@gmail.com>
2018-05-16 19:21:02 +0800

    Update acc.js

--- crud-prac/routes/acc.js
+++ crud-prac/routes/acc.js
@@ -1,8 +1,19 @@
 module.exports = function(app, db){
 	var sess;
+	const dbc = db.db('try');
 	app.get('/acc', (req, res)=>{
 		sess = req.session;
 		console.log(sess.Session);
-		res.send({user: sess.user});
+		const user = { user: sess.user, pass: sess.pass };
+		dbc.collection('accounts').find(user, (err, doc)=>{
+			res.send({id: doc._id, user: doc.user || 'undefined' });
+		})
 	});
+	app.get('/api/check/user', (req, res)=>{
+		sess = req.session;
+		dbc.collection('accounts').find({}).toArray((err, doc)=>{
+			if(err) throw err;
+			res.json(doc);
+		})
+	})
 }
