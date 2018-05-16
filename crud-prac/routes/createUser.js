0ef959890a09ca0c8999c3b9c315c9c2d0716e36
Jm <weeeezyl@gmail.com>
2018-05-16 19:17:02 +0800

    Update createUser.js

--- crud-prac/routes/createUser.js
+++ crud-prac/routes/createUser.js
@@ -1,11 +1,16 @@
 module.exports = function(app, db){
 	const dbc = db.db('try');
+	var sess;
 	app.post('/api/create/user', (req, res)=>{
 		const item = {user: req.body.user, pass: req.body.pass };
-		dbc.collection('usernames').insertOne(item, (err, doc)=>{
+		sess = req.session;
+		sess.user =	item.user; 
+		sess.pass = item.pass; 
+		console.log(item);
+		dbc.collection('accounts').insertOne(item, (err, doc)=>{
 			if(err) throw err;
 			console.log('inserted new row'+doc.insertedCount);
-			res.send(doc)
+			res.send(doc);
 		});
 	});
 }
