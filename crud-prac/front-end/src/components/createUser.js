454309a495157aed593b63486d7d7dfa2571787d
Jm <weeeezyl@gmail.com>
2018-05-16 19:15:29 +0800

    Update createUser.js

--- crud-prac/front-end/src/components/createUser.js
+++ crud-prac/front-end/src/components/createUser.js
@@ -4,11 +4,42 @@ import Header from './header.js';
 
 import { Redirect } from 'react-router';
 
+import { Link } from 'react-router-dom';
+
 class CreateAcc extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = { redirect: false };
+		this.state = { users: [], redirect: false,  };
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e){
+		e.preventDefault();
+		const { users } = this.state;
+		const user = document.getElementById('user');
+		const checker = document.getElementById('checker');
+		users.forEach((item)=>{
+			if(user.value===item.user){
+				checker.innerHTML = 'Username already taken.';
+				user.style.borderRadius = '2px';
+				user.style.border = 'solid 2px red';
+			}
+			else if(user.value === ''){
+				user.placeholder = 'Required Field';
+			}
+			else{
+				if(user.value.length >= 5){
+					checker.innerHTML = 'Username is available.'
+					user.style.borderRadius = '2px';
+					user.style.border = 'solid 2px green';
+				}
+				else{
+					checker.innerHTML = 'Username is too short.'
+					user.style.borderRadius = '2px';
+					user.style.border = 'solid 2px red';
+				}
+			}
+		})
 	}
 	handleSubmit(e){
 		e.preventDefault();
@@ -21,12 +52,22 @@ class CreateAcc extends React.Component{
 			headers: { 'Content-Type' : 'application/json' }
 		}).then(res=>res.ok === true ? this.setState({redirect: true}): console.log('Failed'));
 	}
+	componentDidMount(){
+		fetch('/api/check/user',{
+			credentials: 'include'
+		}).then(res=>res.json()).then(res=>this.setState({users: res}));
+	}
 	render(){
-		const {redirect} = this.state;
+		const { redirect, users } = this.state;
+		if(redirect){
+			return <Redirect to='/' />;
+		}
 			return (<form onSubmit={this.handleSubmit}>
+						<Link to='/'>Go back</Link><br/>
 						<label><strong>Create Account</strong></label><br/>
-						<input type='text' placeholder='Username' name='user' /><br/>
-						<input type='password' placeholder='Password' name='pass' /><br/>
+						<p id='checker'></p>
+						<input type='text' placeholder='Username' name='user' onChange={this.handleChange} id='user' required/><br/>
+						<input type='password' placeholder='Password' name='pass' required/><br/>
 						<input type='submit' value='Submit' />
 					</form>);
 		}
