592476f85cd941a762ce47e0e93e86065fb3cda4
Jm <weeeezyl@gmail.com>
2018-05-17 18:21:24 +0800

    Update acc.js

--- crud-prac/routes/acc.js
+++ crud-prac/routes/acc.js
@@ -1,17 +1,18 @@
+const ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db){
 	var sess;
 	const dbc = db.db('try');
 	app.get('/acc', (req, res)=>{
 		sess = req.session;
-		console.log(sess.Session);
-		const user = { user: sess.user, pass: sess.pass };
+		const user = { id: ObjectID(sess.accId), user: sess.user, pass: sess.pass };
 		dbc.collection('accounts').find(user, (err, doc)=>{
-			res.send({id: doc._id, user: doc.user || 'undefined' });
+			if(err) throw err;
+			res.send({ id: sess.accId, user: sess.user || 'undefined' });
 		})
 	});
 	app.get('/api/check/user', (req, res)=>{
 		sess = req.session;
-		dbc.collection('accounts').find({}).toArray((err, doc)=>{
+		dbc.collection('accounts').find().toArray((err, doc)=>{
 			if(err) throw err;
 			res.json(doc);
 		})
