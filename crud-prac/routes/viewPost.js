fdf0e928766cf459b1bf43d01e29c8927e64d926
Jm <weeeezyl@gmail.com>
2018-05-17 18:21:51 +0800

    Update viewPost.js

--- crud-prac/routes/viewPost.js
+++ crud-prac/routes/viewPost.js
@@ -1,6 +1,9 @@
 module.exports = function(app, db){
 	const dbc = db.db('try');
+	var sess;
 	app.get('/api/posts', (req, res)=>{
+		sess = req.session;
+		console.log(sess);
 		dbc.collection('posts').find({}).toArray((err, docs)=>{
 			if(err) throw err;
 			res.json(docs);
