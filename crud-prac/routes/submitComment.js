55dab09c17f09b4806c8f58a50b662a06e67d2e3
Jm <weeeezyl@gmail.com>
2018-05-18 18:41:53 +0800

    Update submitComment.js

--- crud-prac/routes/submitComment.js
+++ crud-prac/routes/submitComment.js
@@ -1,11 +1,16 @@
+const ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db){
 	const dbc = db.db('try');
+	var sess;
 	app.post('/api/submit/comment', (req, res)=>{
-		console.log(req.body);
-		const items = { postId: req.body.postId, comment: req.body.comment };
-		dbc.collection('comments').insertOne(items, (err, docs)=>{
-			if(err) throw err;
-			res.send('Submitted');
-		})
+		sess = req.session;
+		dbc.collection('accounts').findOne({'_id' : new ObjectID(sess.accId)}, (error,docu)=>{
+			if(error) throw error;
+			const items = { postId: req.body.postId, comment: req.body.comment, userId: docu._id};
+			dbc.collection('comments').insertOne(items, (err, docs)=>{
+				if(err) throw err;
+				res.send('Submitted');
+			});
+		});			
 	})
 };
