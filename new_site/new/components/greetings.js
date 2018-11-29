627e31e95706b2c11d314d15f41ec4182cbd4629
Jm <weeeezyl@gmail.com>
2018-11-29 16:21:25 +0800

    Add files via upload

---
+++ new_site/new/components/greetings.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+function Contents(props){
+	const item = props.item;
+	var idKey = 0;
+	return item.map(res=>{
+		console.log(res);
+		idKey++;
+		return (<div key={idKey}><input type='hidden' value={idKey-1}/><h2># {res.greeting}</h2></div>)
+	})
+}
+
+class GreetingPage extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			isLoaded: false,
+			items: [],
+			error: false
+		}
+	}
+	componentDidMount(){
+		fetch('/greetings',{
+			method: 'GET',
+			credentials: 'include'
+		}).then((res)=>res.json()).then((res)=>this.setState({isLoaded: true, items: res}), (error)=>this.setState({isLoaded: false, error}));
+
+	}
+	render(){
+		const {isLoaded, items, error } = this.state;
+		if(!isLoaded){
+			return <p>Loading...</p>
+		}
+		else if(isLoaded){
+			return <div><h1>Greetings for today!</h1><Contents item={items} /></div>
+		}
+		else{
+			return <p>Error</p>
+		}
+	}
+}
+
+export default GreetingPage;
