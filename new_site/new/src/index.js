264508cd04c87b7376f79669efa8237495ec42b9
Jm <weeeezyl@gmail.com>
2018-12-06 02:46:42 +0800

    Update index.js

--- new_site/new/src/index.js
+++ new_site/new/src/index.js
@@ -1,40 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function Contents(props){
-	const item = props.item;
-	var idKey = 0;
-	return item.map(res=>{
-		console.log(res);
-		idKey++;
-		return (<div key={idKey}><input type='hidden' value={idKey-1}/><h2># {res.greeting}</h2></div>)
-	})
-}
 
-class Page extends Component{
-	constructor(props){
-		super(props);
-		this.state = {isLoaded: false , items: [] , error: false}
-	}
-	componentDidMount(){
-		fetch('/greet',{
-			method: 'GET',
-			credentials: 'include'
-		}).then((res)=>res.json()).then((res)=>this.setState({isLoaded: true, items: res}), (error)=>this.setState({isLoaded: false, error}));
-	}
+//components
+import GreetingPage from './components/greetings.js';
+import AddGreetings from './components/addGreeting.js';
+
+class MainPage extends Component{
 	render(){
-		const {isLoaded, items, error } = this.state;
-		if(!isLoaded){
-			return <p>Loading...</p>
-		}
-		else if(isLoaded){
-			return <div><h1>Greetings for today!</h1><Contents item={items} /></div>
-		}
-		else{
-			return <p>Error 200</p>
-		}
+		return (
+			<Router>
+			<div>
+			<Route exact path='/' component={GreetingPage} />
+			<Route path='/:addgreet' component={AddGreetings} />
+			</div>
+			</Router>)
 	}
 }
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+ReactDOM.render(<MainPage />, document.getElementById('root'));
